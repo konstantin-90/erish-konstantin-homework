be0c17137c5a36064a89413813d8372c4af1ecbd
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-02-07 11:04:43 +0200

    create file and add

---
+++ yerysh-konstantin-03/main.js
@@ -0,0 +1,12 @@
+let firstName = prompt ('enter your first name');
+let lastName = prompt ('enter your last name');
+let age = prompt ('enter your age');
+
+alert (`Hello ${firstName} ${lastName} your age ${age}`);
+
+
+let yourNumber = prompt ("enter five-digit number");
+let yourMasiveNumber = yourNumber.split("");
+let yourMasiveString = yourMasiveNumber.join(" ");
+
+alert (yourMasiveString);
