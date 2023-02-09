24797273a24424b810ce545cc9961b23d7195d40
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-02-09 12:35:11 +0200

    change homwork number 8

--- yerysh-konstantin-08/main.js
+++ yerysh-konstantin-08/main.js
@@ -1,49 +1,45 @@
 const yearBirth = prompt ('enter your year of birth');
 
 const yourCity = prompt('enter your city where you live');
-switch (true) {
-  case yourCity ==='Киев':
-    alert('ты живешь в столице Украины');
-    break;
+let place;
 
-  case yourCity === 'Вашингтон':
-    alert('ты живешь в столице США');
-    break;
-
-  case yourCity === 'Лондон':
-    alert('ты живешь в столице Англии');
-    break;
-
-  case yourCity === 'string':
-    alert(`ты живешь в городе + ${yourCity}`);
-    break;
-
-  case yourCity === null:
-    alert('жаль, что не захотел ввести свой город');
-    break;
+if (!yourCity) {
+  alert('жаль, что не захотел ввести свой любивый вид спорта');
+} else {
+  switch (yourCity) {
+    case "Киев":
+      place = 'ты живешь в столице Украины';
+      break;
+    case "Вашингтон":
+      place = 'ты живешь в столице США';
+      break;
+    case 'Лондон':
+      place = 'ты живешь в столице Англии';
+      break;
+    default:
+      place = `ты живешь в городе + ${yourCity}`;
+  }
 }
 
-let likeSport = prompt ('enter your favorite sport(бокс, легкая атлетика, футбол)');
-switch (true) {
-  case likeSport ==='бокс':
-    alert('ты живешь в столице Украины');
-    break;
+const likeSport = prompt('enter your favorite sport(бокс, легкая атлетика, футбол)');
+let favSport;
 
-  case likeSport === 'легкая атлетика':
-    alert('ты живешь в столице США');
-    break;
-
-  case likeSport === 'футбол':
-    alert('ты живешь в столице Англии');
-    break;
-
-  case likeSport === 'string':
-    alert(`твой любимый вид спорта + ${likeSport}`);
-    break;
-
-  case likeSport === null:
-    alert('жаль, что не захотел ввести свой любивый вид спорта');
-    break;
+if (!likeSport) {
+  alert('жаль, что не захотел ввести свой любивый вид спорта');
+} else {
+  switch (likeSport) {
+    case "бокс":
+      favSport = 'круто! Хочешь стать как Мухаммед Али';
+      break;
+    case 'футбол':
+    favSport = 'круто! Хочешь стать как Лионель Месси';
+      break;
+    case 'легкая атлетика':
+    favSport = 'круто! Хочешь стать как Усэйн Болт';
+      break;
+    default:
+      favSport = `Круто, что твой любимый вид спорта ${likeSport}`;
+  }
 }
 
 alert (`твоя дата рождения ${yearBirth}, ты живешь в городе ${yourCity}. Твоим любимым видом спорта является ${likeSport}`);
