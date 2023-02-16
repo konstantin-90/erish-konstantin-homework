1effc8c2217eb29991c430abaa075746ebfee1c8
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-02-16 17:53:10 +0200

    complite work 20

--- yerysh-konstantin-13/main.js
+++ yerysh-konstantin-13/main.js
@@ -120,24 +120,24 @@
 
 
 // ---------------------Знайти найбільший серед елементів масиву, осnальні обнулити.------------------
-let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let max = [];
+// let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+// let max = [];
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > max) {
-    max = arr[i];
-  } 
-}
-console.log(max);
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   } 
+// }
+// console.log(max);
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] != max) {
-    arr[i] = 0;
-  } 
-}
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] != max) {
+//     arr[i] = 0;
+//   } 
+// }
 
-console.log(arr);
+// console.log(arr);
 
-arr.splice(0, arr.length, max);
+// arr.splice(0, arr.length, max);
 
-console.log(arr);
+// console.log(arr);
