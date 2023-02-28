8271a4d85d9d91437373ff3562c7252cf5dd541b
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-02-28 12:30:11 +0200

    complite 25

--- yerysh-konstantin-25/main.js
+++ yerysh-konstantin-25/main.js
@@ -4,3 +4,45 @@
 // При введенні znak потрібно перевірити коректність введення на можливі математичні дії.
 // p = new SuperMath();
 // p.check(obj); // --> no p.input() -> 3 prompt -> рахує
+
+class SuperMath {
+  check(obj) {
+    const { X, Y, znak } = obj;
+
+    if (this.isValidZnak(znak)) {
+      const result = this.calculate(X, Y, znak);
+      console.log(`Result: ${result}`);
+    } else {
+      console.log("Invalid input!");
+      this.input();
+    }
+  }
+
+  isValidZnak(znak) {
+    const validZnaks = ["+", "-", "/", "*", "%"];
+    return validZnaks.includes(znak);
+  }
+
+  calculate(X, Y, znak) {
+    switch (znak) {
+      case "+":
+        return X + Y;
+      case "-":
+        return X - Y;
+      case "/":
+        return X / Y;
+      case "*":
+        return X * Y;
+      case "%":
+        return X % Y;
+    }
+  }
+
+  input() {
+    const X = prompt("Enter X:");
+    const Y = prompt("Enter Y:");
+    const znak = prompt("Enter znak (+, -, /, *, %):");
+    const obj = { X: Number(X), Y: Number(Y), znak };
+    this.check(obj);
+  }
+}
