57e64d15bc69c983cc17ca6b02320313b07f10bb
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-02-28 12:36:15 +0200

    complite 15

--- yerysh-konstantin-15/main.js
+++ yerysh-konstantin-15/main.js
@@ -1,24 +1,17 @@
 // Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
 
-// Наприклад:
-
-// const array = [1, 2, 3, 4, 5, 6, 7];
-// removeElement(array, 5 );
-// console.log(array);
-// // Результат: [1, 2, 3, 4, 6, 7]
-
-
 const array = [1, 2, 3, 'sg4', 5, 'gh', {}];
 
 
-function removeElement(anyArr, index) { // прописал функцию. С нужным названием
-    const result = anyArr.splice(index - 1, 1); // прописал новую переменую, которая обчисляется через метод splice. Находит индекс, который вводит пользователь(item) и минус 1 индекс.
-
-    return result;
+function removeElement(array, item) {
+    const index = array.indexOf(item); 
+    if (index !== -1) { 
+      array.splice(index, 1); 
+    }
+    return array; 
 }
 
-removeElement(array, 7);
+removeElement(array, 'sg4');
 
 console.log(array);
-
