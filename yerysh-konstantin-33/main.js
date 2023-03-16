c51bb3840189d8797f2ed51e24722ee03ed6e46f
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-03-16 22:18:53 +0200

    complite 33

--- yerysh-konstantin-33/main.js
+++ yerysh-konstantin-33/main.js
@@ -1,31 +1,23 @@
 function generateList(array) {
-    // Створюємо елемент списку
-    const list = document.createElement('ul');
-  
-    // Перебираємо масив
-    for (let i = 0; i < array.length; i++) {
-      // Створюємо елемент списку
-      const listItem = document.createElement('li');
-  
-      // Якщо елемент масиву є масивом
-      if (Array.isArray(array[i])) {
-        // Рекурсивно генеруємо список з елементів масиву
-        const sublist = generateList(array[i]);
-        // Додаємо список до елементу списку
-        listItem.appendChild(sublist);
-      } else {
-        // Якщо елемент масиву є числом
-        // Створюємо текстовий вузол з числом
-        const text = document.createTextNode(array[i]);
-        // Додаємо текстовий вузол до елементу списку
-        listItem.appendChild(text);
-      }
-  
-      // Додаємо елемент списку до списку
-      list.appendChild(listItem);
+  const list = document.createElement('ul');
+
+  for (let i = 0; i < array.length; i++) {
+    const item = document.createElement('li');
+
+    if (typeof array[i] === 'number') {
+      item.textContent = array[i];
     }
-  
-    // Повертаємо список
-    return list;
+    else if (Array.isArray(array[i])) {
+      item.appendChild(generateList(array[i]));
+    }
+
+    list.appendChild(item);
   }
-  
+
+  return list;
+}
+
+
+const myArray = [1, 2, [1, 2, 3], 3];
+const myList = generateList(myArray);
+document.body.appendChild(myList);
