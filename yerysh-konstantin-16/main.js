de5ee03faca4a7d31ef27edb8f283672d9a47436
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-03-20 10:34:27 +0200

    complite 16

--- yerysh-konstantin-16/main.js
+++ yerysh-konstantin-16/main.js
@@ -12,13 +12,17 @@
 
 
 
-function* generateKey(length, characters) {
+function generateKey(length, characters) {
+  let result = '';
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
-    yield characters(Math.floor(Math.random() * charactersLength));
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex);
   }
+  return result;
 }
 
+
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-const key = [...generateKey(16, characters)].join('');
+const key = generateKey(16, characters);
 console.log(key); 
