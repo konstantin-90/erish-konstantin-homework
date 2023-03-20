dc65f9c0ec22954720746e9bfc0bfad9319e4c4d
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-03-20 10:40:17 +0200

    complite 17

--- yerysh-konstantin-17/main.js
+++ yerysh-konstantin-17/main.js
@@ -1,62 +1,89 @@
 //     Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
-let arr = ['a', 2, 'y', '34', 12, 'afaf', 9];
+const arr = [1, 2, 'a', 3, 'b', 4];
 
-function calculateArr(arr) { // функция с  названием calculate. принимает масив с названием arr
-    let sum = 0; // переменная sum, которая изначально равна 0. считать сумму числе
-    let count = 0; // переменная count, которая изначально равна 0. Будет считать количество интераций
-    
-    for (let i = 0; i < arr.length; i++) { // цикл, который перебирает масив arr по его длине
-      if (typeof arr[i] === 'number') { //смотрит на условие, если элемент по типу равен number
-        sum += arr[i]; // прибавляет число в переменую sum
-        count++; // считает количество таких "успешных" интераций
-        return sum / count;
-      }
+function calculateAverage(arr) {
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+      sum += arr[i];
+      count++;
     }
-    
-    // if (count > 0) { // проверка на полодительное число
-    //   return sum / count; // если число больше 0, возвращает значение sum(сумма всех чисел) деленное на count(количество интераций)
-    // } else {
-    //   return 0;
-    // }
   }
+  return count > 0 ? sum / count : 0;
+}
 
-  let arr2 = calculateArr(arr);
-  console.log(arr2); 
-  
+const avg = calculateAverage(arr); 
+
+
 
 //     Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
 
 function doMath(x, znak, y) {
-    let result;
-    switch (znak) {
-      case '+':
-        result = x + y;
-        break;
-      case '-':
-        result = x - y;
-        break;
-      case '*':
-        result = x * y;
-        break;
-      case '/':
-        result = x / y;
-        break;
-      case '%':
-        result = x % y;
-        break;
-      case '^':
-        result = Math.pow(x, y);
-        break;
-      default:
-        result = 'wrong comand';
-    }
-    return result;
+  let result;
+  switch (znak) {
+    case '+':
+      result = x + y;
+      break;
+    case '-':
+      result = x - y;
+      break;
+    case '*':
+      result = x * y;
+      break;
+    case '/':
+      result = x / y;
+      break;
+    case '%':
+      result = x % y;
+      break;
+    case '^':
+      result = Math.pow(x, y);
+      break;
+    default:
+      result = NaN;
   }
+  return result;
+}
 
-  console.log(doMath(3, '*', 5));
+const x = 5;
+const y = 3;
+const znak = '*';
+const result = doMath(x, znak, y);
+
 
 //     Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
 
+// function fillArray(rows, cols) {
+//   const arr = [];
+//   for (let i = 0; i < rows; i++) {
+//     arr[i] = [];
+//     for (let j = 0; j < cols; j++) {
+//       arr[i][j] = prompt(`Enter value for arr[${i}][${j}]:`);
+//     }
+//   }
+//   return arr;
+// }
+
+// const rows = prompt('Enter number of rows:');
+// const cols = prompt('Enter number of columns:');
+// const arr = fillArray(rows, cols);
+
+
 
 //     Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
+
+// function removeChars(str, charsToRemove) {
+//   let result = '';
+//   for (let i = 0; i < str.length; i++) {
+//     if (!charsToRemove.includes(str[i])) {
+//       result += str[i];
+//     }
+//   }
+//   return result;
+// }
+
+// const str = ' hello world';
+// const charsToRemove = ['l', 'd'];
+// const result = removeChars(str, charsToRemove); 
 
