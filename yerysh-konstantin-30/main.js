3384270beecf0fc4ac2c8f06b278f2592fd57a09
konstantin-90 <Rahxephonphoenix@gmail.com>
2023-03-27 12:18:03 +0300

    complite 30

--- yerysh-konstantin-30/main.js
+++ yerysh-konstantin-30/main.js
@@ -1,42 +1,10 @@
-const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-const btn3 = document.getElementById('btn3');
-const btn4 = document.getElementById('btn4');
-const btn5 = document.getElementById('btn5');
+const wrapBtn = document.querySelector('.wrapper');
 
-const count1 = document.getElementById('count1');
-const count2 = document.getElementById('count2');
-const count3 = document.getElementById('count3');
-const count4 = document.getElementById('count4');
-const count5 = document.getElementById('count5');
-
-let voteCount1 = 0;
-let voteCount2 = 0;
-let voteCount3 = 0;
-let voteCount4 = 0;
-let voteCount5 = 0;
-
-btn1.addEventListener('click', () => {
-  voteCount1++;
-  count1.innerHTML = voteCount1;
-});
-
-btn2.addEventListener('click', () => {
-  voteCount2++;
-  count2.innerHTML = voteCount2;
-});
-
-btn3.addEventListener('click', () => {
-  voteCount3++;
-  count3.innerHTML = voteCount3;
-});
-
-btn4.addEventListener('click', () => {
-  voteCount4++;
-  count4.innerHTML = voteCount4;
-});
-
-btn5.addEventListener('click', () => {
-  voteCount5++;
-  count5.innerHTML = voteCount5;
+wrapBtn.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.tagName === 'BUTTON') {
+    const countEl = target.nextElementSibling;
+    const count = Number(countEl.textContent);
+    countEl.textContent = count + 1;
+  }
 });
